9033a990972e977596a89a7ec81cd19b84b28fed
hyos <hyozzang99@gmail.com>
2025-12-07 23:43:05 +0900

    Init: 프로젝트 초기 설정 및 기본 환경 구성

---
+++ src/test/java/com/blink/blink/BlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blink.blink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
