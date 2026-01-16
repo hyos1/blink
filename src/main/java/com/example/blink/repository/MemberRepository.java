d8b0f872fd074f7c6b2991c153b603d312c11320
hyos <hyozzang99@gmail.com>
2026-01-16 23:44:38 +0900

    Feat: 피드 용 내 프로필 조회와 회원 프로필 조회 API 분리

---
+++ src/main/java/com/example/blink/repository/MemberRepository.java
@@ -0,0 +1,38 @@
+package com.example.blink.repository;
+
+import com.example.blink.domain.Member;
+import com.example.blink.service.member.response.MemberProfileDto;
+import com.example.blink.service.member.response.MemberSidebarDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+
+    @Query("select new com.example.blink.service.member.response.MemberSidebarDto(" +
+            "m.id, m.name, m.email, " +
+            "(select count(p) from Post p where p.member = m), " +
+            "(select count(f) from Follow f where f.following = m), " +
+            "(select count(f) from Follow f where f.follower = m)" +
+            ") " +
+            "from Member m " +
+            "where m.id = :memberId")
+    Optional<MemberSidebarDto> findSidebarInfoById(@Param("memberId") Long memberId);
+
+    // 기본 프로필 정보만 조회
+    @Query("select new com.example.blink.service.member.response.MemberProfileDto(" +
+            "m.id, m.name, m.bio, " +
+            "(select count(p) from Post p where p.member = m), " +
+            "(select count(f) from Follow f where f.following = m), " +
+            "(select count(f) from Follow f where f.follower = m)" +
+            ") " +
+            "from Member m " +
+            "where m.id = :memberId")
+    Optional<MemberProfileDto> findProfileById(@Param("memberId") Long memberId);
+
+    @Query("select m.id from Member m where m.username = :username")
+    Optional<Long> findIdByUsername(String username);
+}
