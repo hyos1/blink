37a0aa093249c777eb441953c406f5bd2500fb91
hyos <hyozzang99@gmail.com>
2026-02-03 16:15:10 +0900

    Feat: Feed 화면에서 보여줄 게시물 페이징 처리하여 데이터 모델에 추가

--- src/main/java/com/example/blink/web/HomeController.java
+++ src/main/java/com/example/blink/web/HomeController.java
@@ -1,15 +1,19 @@
 package com.example.blink.web;
 
+import com.example.blink.repository.post.query.FeedPostDto;
 import com.example.blink.service.login.response.LoginMember;
 import com.example.blink.service.member.response.MemberSidebarDto;
 import com.example.blink.service.member.MemberService;
+import com.example.blink.service.post.PostService;
 import com.example.blink.web.dto.LoginForm;
 import com.example.blink.web.session.SessionConst;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
@@ -17,11 +21,14 @@ import org.springframework.web.bind.annotation.SessionAttribute;
 public class HomeController {
 
     private final MemberService memberService;
+    private final PostService postService;
 
     //사용자의 JSESSIONID 속성 이름 중 LOGIN_MEMBER 있으면 value할당, 없으면 null
     @GetMapping("/")
     public String homeV1(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) LoginMember loginMember,
-                         @ModelAttribute("loginForm") LoginForm form, Model model) {
+                         @ModelAttribute("loginForm") LoginForm form,
+                         @RequestParam(defaultValue = "0") int page,
+                         Model model) {
 
         //LoginMember는 (id, name)만 존재
         // 로그인 안 돼있으면 로그인 페이지로 이동
@@ -34,6 +41,10 @@ public class HomeController {
         // 왼쪽 사이드바: 내 프로필 정보
         MemberSidebarDto myProfile = memberService.getMemberSidebarDto(loginMember.getId());
         model.addAttribute("myProfile", myProfile);
+
+        // 피드 게시물들 (페이징)
+        Page<FeedPostDto> posts = postService.getFeedPosts(loginMember.getId(), page, 3);
+        model.addAttribute("posts", posts);
 
         return "feed";
     }
