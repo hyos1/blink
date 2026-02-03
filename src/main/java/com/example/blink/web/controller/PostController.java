fbe6de490183ac595caaeacdd9d2d9823228f9a2
hyos <hyozzang99@gmail.com>
2026-02-03 16:17:04 +0900

    Feat: Feed 화면에서 AJAX로 게시물 추가 조회 컨트롤러 구현 - 스크롤 내리면서 게시물 추가 조회 시 페이징 처리 후 fragment 반환 - 비동기 요청으로 게시물 fragment만 갱신 가능

--- src/main/java/com/example/blink/web/controller/PostController.java
+++ src/main/java/com/example/blink/web/controller/PostController.java
@@ -2,13 +2,16 @@ package com.example.blink.web.controller;
 
 import com.example.blink.file.FileStore;
 import com.example.blink.file.request.UploadFile;
+import com.example.blink.repository.post.query.FeedPostDto;
 import com.example.blink.service.login.response.LoginMember;
 import com.example.blink.service.post.PostService;
 import com.example.blink.service.post.request.CreatePostCommand;
 import com.example.blink.web.dto.CreatePostForm;
 import com.example.blink.web.session.SessionConst;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -51,5 +54,21 @@ public class PostController {
         postService.addPost(command);
 
         return "redirect:/members/profile/me";
+    }
+
+    // 피드 화면 게시물 조회
+    @GetMapping("/feed")
+    public String getFeedPosts(
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER) LoginMember loginMember,
+            @RequestParam(defaultValue = "0") int page,
+            Model model
+    ) {
+        // 게시물은 3개씩 페이징
+        Page<FeedPostDto> posts = postService.getFeedPosts(loginMember.getId(), page, 3);
+
+        model.addAttribute("posts", posts);
+
+        // fragments/post-item.html의 post-list fragment 반환
+        return "fragments/post-item :: post-list";
     }
 }
