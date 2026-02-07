8faea52ff7fb2c4b9267def8b9b5080c51c4dfd5
hyos <hyozzang99@gmail.com>
2026-02-07 19:53:09 +0900

    Feat: API 전용 예외 처리 핸들러 추가 - ClientException, ServerException 처리 로직 구현 - "API 컨트롤러만" RestControllerAdvice 적용 - 공통 Json 응답 구조 정의

---
+++ src/main/java/com/example/blink/exhandler/ApiExceptionHandler.java
@@ -0,0 +1,58 @@
+package com.example.blink.exhandler;
+
+import com.example.blink.exception.ClientException;
+import com.example.blink.exception.ServerException;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice(basePackages = "com.example.blink.web.controller.api")
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(ClientException.class)
+    public ResponseEntity<ErrorResponse> handleClientException(ClientException e) {
+        log.warn("ClientException: ", e.getMessage());
+
+        ErrorResponse response = new ErrorResponse(e.getErrorCode().name(), e.getMessage());
+        return ResponseEntity
+                .status(e.getErrorCode().getHttpStatus())
+                .body(response);
+    }
+
+    @ExceptionHandler(ServerException.class)
+    public ResponseEntity<ErrorResponse> handleServerException(ServerException e) {
+        log.error("ServerException:", e.getMessage(), e);
+
+        ErrorResponse response = new ErrorResponse(
+                "SERVER_ERROR", "서버 오류가 발생했습니다."
+        );
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
+    }
+
+    // 내가 정의하지 못한 예외
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleException(Exception e) {
+        log.error("예상치 못한 오류: ", e.getMessage(), e);
+
+        ErrorResponse response = new ErrorResponse(
+                "UNKNOWN_ERROR", "알 수 없는 오류가 발생했습니다."
+        );
+
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ErrorResponse {
+        private String code;
+        private String message;
+    }
+}
