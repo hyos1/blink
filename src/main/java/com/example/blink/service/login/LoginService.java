faed5a2d293e8db7ffc7f3cab563edb7b91d7fab
hyos <hyozzang99@gmail.com>
2026-02-08 16:03:41 +0900

    Refactor: LoginService 로그인 실패 시 예외 대신 null 반환 - 500 오류 페이지 노출됐던 것을 null을 반환 -> 컨트롤러에서 BindingResult로 화면에서 오류 메세지 표시 정상 출력 확인

--- src/main/java/com/example/blink/service/login/LoginService.java
+++ src/main/java/com/example/blink/service/login/LoginService.java
@@ -1,6 +1,8 @@
 package com.example.blink.service.login;
 
 import com.example.blink.domain.Member;
+import com.example.blink.exception.ClientException;
+import com.example.blink.exhandler.ErrorCode;
 import com.example.blink.repository.member.MemberRepository;
 import com.example.blink.service.login.request.LoginCommand;
 import com.example.blink.service.login.response.LoginMember;
@@ -9,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.example.blink.exhandler.ErrorCode.*;
 
 @Slf4j
 @Service
@@ -22,12 +26,18 @@ public class LoginService {
     @Transactional
     public LoginMember login(LoginCommand loginCommand) {
 
+        // 컨트롤러에서 BindingResult로 처리하기 위해 null 반환
         Member member = memberRepository.findByEmail(loginCommand.getEmail())
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 회원입니다,"));
+                .orElse(null);
 
+        if (member == null) {
+            return null;
+        }
+
+        // 입력한 비밀번호와 찾은 회원의 비밀번호가 같아야함
         if (!passwordEncoder.matches(loginCommand.getPassword(), member.getPassword())) {
-            log.info("비밀번호 오류");
-            throw new IllegalArgumentException("비밀번호가 맞지 않습니다.");
+            log.info("비밀번호가 일치하지 않습니다.");
+            return null;
         }
 
         return new LoginMember(member.getId(), member.getName());
